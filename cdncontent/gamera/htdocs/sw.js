9099f455b265ceb25ead9121b8db153e41b79e76
entlein <einentlein@gmail.com>
2024-11-22 21:00:53 +0100

    CORS issues 21

--- cdncontent/gamera/htdocs/sw.js
+++ cdncontent/gamera/htdocs/sw.js
@@ -1,40 +1,48 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(self.skipWaiting());
-  });
-  
-  self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      fetch(event.request).then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const newHeaders = new Headers(response.headers);
+      newHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
+      newHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
+      return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: newHeaders,
+      });
+    }).catch((error) => {
+      console.error('Fetch failed; returning offline page instead.', error);
+      return caches.match(event.request).then((response) => {
+        if (response) {
+          return response;
+        } else {
+          return new Response('Service is offline', {
+            status: 503,
+            statusText: 'Service Unavailable',
+            headers: new Headers({
+              'Content-Type': 'text/plain'
+            })
+          });
         }
-        const newHeaders = new Headers(response.headers);
-        newHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
-        newHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
-        return new Response(response.body, {
-          status: response.status,
-          statusText: response.statusText,
-          headers: newHeaders,
+      }).catch(() => {
+        return new Response('Service is offline', {
+          status: 503,
+          statusText: 'Service Unavailable',
+          headers: new Headers({
+            'Content-Type': 'text/plain'
+          })
         });
-      }).catch((error) => {
-        console.error('Fetch failed; returning offline page instead.', error);
-        return caches.match(event.request).then((response) => {
-          if (response) {
-            return response;
-          } else {
-            return new Response('Service is offline', {
-              status: 503,
-              statusText: 'Service Unavailable',
-              headers: new Headers({
-                'Content-Type': 'text/plain'
-              })
-            });
-          }
-        });
-      })
-    );
-  });
+      });
+    })
+  );
+});
